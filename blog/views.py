36f4b805929cc98a79a4f19945e93e904803beb1
gitforsck <hyeona.kim@statschippac.com>
2018-12-13 12:09:29 +0900

    Added view and temeplate for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 # from 다음에 있는 마침표(.)는 현재 디렉토리 또는 애플리케이션을 의미
 from .models import Post
 from django.utils import timezone
-
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',{'posts' : posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
