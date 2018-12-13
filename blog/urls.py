2ffe0e280d412c5b45d488ce01e488689d12ede0
gitforsck <hyeona.kim@statschippac.com>
2018-12-13 14:11:25 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url('^$', views.post_list, name="post_list"),
     url('^post/(?P<pk>\d+)/$', views.post_detail, name="post_detail"),
+    url('^post/new/$', views.post_new, name='post_new'),
+    url('^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
